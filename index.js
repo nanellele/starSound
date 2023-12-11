7ad4c7304e5956728b7cebe0e56e272a66444004
Nanelle Le <nanellele@gmail.com>
2023-12-11 04:20:31 -0500

    Add functionality for playing videos

--- index.js
+++ index.js
@@ -1,80 +1,162 @@
+// DOM elements
 const image = document.getElementById('cover'),
-title = document.getElementById('music-title'),
-artist = document.getElementById('music-artist'),
-currentTimeEl = document.getElementById('current-time'),
-durationEl = document.getElementById('duration'),
-progress = document.getElementById('progress'),
-playerProgress = document.getElementById('player-progress'),
-prevBtn = document.getElementById('prev'),
-playBtn = document.getElementById('play'),
-nextBtn = document.getElementById('next'),
-background = document.getElementById('bg-img');
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    playBtn = document.getElementById('play'),
+    nextBtn = document.getElementById('next'),
+    background = document.getElementById('bg-img');
 
+// Load the YouTube API asynchronously
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+// Audio and playlist
 const music = new Audio();
+// const songs = [
+//     {
+//         path: 'assets/1.mp3',
+//         displayName: 'Cosmic Star Candy',
+//         cover: 'assets/starry.jpg',
+//         artist: 'Daystar',
+//         type: 'audio'
+//     },
+// ];
 
-const songs = [
+
+const media = [
     {
-        path: 'assets/1.mp3',
-        displayName: 'Song 1',
-        cover: 'assets/starry.jpg',
-        artist: 'Song 1',
-    },
-    {
-        path: 'assets/1.mp3',
-        displayName: '',
+        path: '',
+        displayName: 'Cosmic Star Candy',
         cover: '',
-        artist: '',
-    },
-    {
-        path: 'assets/1.mp3',
-        displayName: '',
-        cover: '',
-        artist: '',
+        id: "eq3C1Uwz6YU",
+        artist: 'Daystar',
+        type: 'video'
     }
 ];
 
 let musicIndex = 0;
 let isPlaying = false;
 
-function togglePlay(){
-    if(isPlaying){
-        pauseMusic();
-    }
-    else{
-        playMusic();
-    }
+// Toggle play/pause
+function togglePlay() {
+    isPlaying ? pauseMusic() : playMusic();
 }
 
-function playMusic(){
+// Play music
+function playMusic() {
     isPlaying = true;
     playBtn.classList.replace('fa-play', 'fa-pause');
     playBtn.setAttribute('title', 'Pause');
-    music.play();
 }
 
-function pauseMusic(){
+// Pause music
+function pauseMusic() {
     isPlaying = false;
     playBtn.classList.replace('fa-pause', 'fa-play');
     playBtn.setAttribute('title', 'Play');
-    music.pause();
 }
 
-function loadMusic(song){
-    music.src = song.path;
-    title.textContent = song.displayName;
-    artist.textContent = song.artist;
-    image.src = song.cover;
-    background.src = song.cover;
+// Load music details (for both audio and YouTube video)
+function loadMedia(media) {
+    if (media.type === 'audio') {
+        music.src = media.path;
+        title.textContent = media.displayName;
+        artist.textContent = media.artist;
+        image.src = media.cover;
+        background.src = media.cover;
+    } else if (media.type === 'video') {
+        title.textContent = media.displayName;
+        artist.textContent = media.artist;
+    }
 }
 
-function changeMusic(direction){
-    musicIndex = (musicIndex + direction + songs.length) % songs.length;
-    loadMusic(songs[musicIndex]);
+// Change media based on direction (-1 for previous, 1 for next)
+async function changeMedia(direction) {
+    // const currentMedia = getCurrentMedia();
+    musicIndex = (musicIndex + direction + media.length) % media.length;
+    console.log(musicIndex)
+    console.log(media[musicIndex])
+    loadMedia(media[musicIndex]);
+    player.loadVideoById(media[musicIndex].id, 0, "large");
     playMusic();
 }
 
+// Function to get the currently playing media (audio or video)
+// function getCurrentMedia() {
+//     return isPlaying ? media[musicIndex] : { type: 'video', videoTitle: 'YouTube Video', videoArtist: 'YouTube' };
+// }
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '300',
+        width: '300',
+        videoId: 'eq3C1Uwz6YU',
+        playerVars: {
+            playsinline: 1,
+            origin: 'https://www.youtube.com'
+        },
+        origin: 'http://127.0.0.1:5501/',
+        // Add your API key here
+
+        // AIzaSyDcrsvYDdftdtCEk17gOW7_AG-0VKlyXc
+        apiKey: 'AIzaSyDcrsvYDdftdtCEk17gOW7_AG-0VKlyXcE',
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange
+        }
+    });
+}
+
+// YouTube video ID
+// const videoID = "sVTy_wmn5SU";
+
+async function search(){
+    const searchTerm = document.getElementById("searchBox").value;
+    const api_url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&key=AIzaSyDcrsvYDdftdtCEk17gOW7_AG-0VKlyXcE`
+    
+    // Send request to grab data for first video in search
+    const res = await fetch(api_url)
+    const data = await res.json()
+
+    console.log(api_url)
+
+    // Grab videoId from response
+    const videoId = data.items[0].id.videoId
+
+    console.log(videoId)
+
+    const newMedia = {
+        path: '',
+        displayName: data.items[0].snippet.title,
+        cover: '',
+        id: videoId,
+        artist: data.items[0].snippet.channelTitle,
+        type: 'video'
+    }
+
+    media.push(newMedia)
+    console.log(media)
+
+    // Skip to the latest added song
+    changeMedia(media.length-1);
+    // Load video into player
+    // player.loadVideoById(videoId, 0, "large")
+    // playMusic();
+}
+
+
+// Update progress bar and display
 function updateProgressBar() {
-    const { duration, currentTime } = music;
+    const { duration, currentTime } = music; // Assuming music is your audio player
     const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
 
@@ -83,17 +165,49 @@ function updateProgressBar() {
     currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
 }
 
-function setProgressBar (e) {
+
+// Set progress bar based on user click
+function setProgressBar(e) {
     const width = playerProgress.clientWidth;
     const clickX = e.offsetX;
     music.currentTime = (clickX / width) * music.duration;
 }
 
-playBtn.addEventListener('click', togglePlay);
-prevBtn.addEventListener('click', () => changeMusic(-1));
-nextBtn.addEventListener('click', () => changeMusic(1));
-music.addEventListener('ended', () => changeMusic(1));
-music.addEventListener('timeupdate', updateProgressBar);
-playerProgress.addEventListener('click', setProgressBar);
+// Event handlers for YouTube player
+function onPlayerReady(e) {
+    // Optional: Logic when the YouTube player is ready
+}
 
-loadMusic(songs[musicIndex]);
+function onPlayerStateChange(e) {
+    // Optional: Logic when the YouTube player's state changes
+}
+
+//scrolling effect for music title
+function startScrolling() {
+    const containerWidth = musicTitleContainer.offsetWidth;
+    const titleWidth = musicTitle.offsetWidth;
+
+    if (titleWidth > containerWidth) {
+        musicTitle.style.animation = `scroll ${titleWidth / 50}x linear infinite`;
+    }
+}   
+
+// Event listener for play/pause button for YouTube player
+playBtn.addEventListener('click', () => {
+    if (player.getPlayerState() === YT.PlayerState.PLAYING) {
+        player.pauseVideo();
+    } else {
+        player.playVideo();
+    }
+});
+
+// Event listeners
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMedia(-1));
+nextBtn.addEventListener('click', () => changeMedia(1));
+music.addEventListener('timeupdate', updateProgressBar);
+// playerProgress.addEventListener('click', setProgressBar);
+
+loadMedia(media[musicIndex])
+
+startScrolling();
