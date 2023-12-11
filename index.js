c23677725d918b6ea1f03ac809f1e283d666e6b2
Nanelle Le <nanellele@gmail.com>
2023-12-11 07:47:25 -0500

    Update YouTube player and search functionality

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const image = document.getElementById('cover'),
     playBtn = document.getElementById('play'),
     nextBtn = document.getElementById('next'),
     background = document.getElementById('bg-img');
+    musicTitle = document.getElementById('music-title');
+    musicTitleContainer = document.getElementById('music-title-container');
+    volume = document.getElementById('volume');
+
+let results = [];
+let currentTime, duration = 0;
+
+const key = 'AIzaSyBmj_PKt5gPNQ4Q6v-bHS-d9s-i6wASKRU'
 
 // Load the YouTube API asynchronously
 var tag = document.createElement('script');
@@ -50,11 +58,14 @@ function togglePlay() {
     isPlaying ? pauseMusic() : playMusic();
 }
 
+let looper;
+
 // Play music
 function playMusic() {
     isPlaying = true;
     playBtn.classList.replace('fa-play', 'fa-pause');
     playBtn.setAttribute('title', 'Pause');
+    looper = setInterval(updateProgressBar, 1000);
 }
 
 // Pause music
@@ -62,6 +73,7 @@ function pauseMusic() {
     isPlaying = false;
     playBtn.classList.replace('fa-pause', 'fa-play');
     playBtn.setAttribute('title', 'Play');
+    clearInterval(looper);
 }
 
 // Load music details (for both audio and YouTube video)
@@ -102,13 +114,16 @@ function onYouTubeIframeAPIReady() {
         videoId: 'eq3C1Uwz6YU',
         playerVars: {
             playsinline: 1,
+            'controls': 0,
+            'autoplay': 1,
+            modestbranding: 1,
             origin: 'https://www.youtube.com'
         },
         origin: 'http://127.0.0.1:5501/',
         // Add your API key here
 
         // AIzaSyDcrsvYDdftdtCEk17gOW7_AG-0VKlyXc
-        apiKey: 'AIzaSyDcrsvYDdftdtCEk17gOW7_AG-0VKlyXcE',
+        apiKey: key,
         events: {
             onReady: onPlayerReady,
             onStateChange: onPlayerStateChange
@@ -121,33 +136,15 @@ function onYouTubeIframeAPIReady() {
 
 async function search(){
     const searchTerm = document.getElementById("searchBox").value;
-    const api_url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&key=AIzaSyDcrsvYDdftdtCEk17gOW7_AG-0VKlyXcE`
+    const api_url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&key=${key}`
     
     // Send request to grab data for first video in search
     const res = await fetch(api_url)
     const data = await res.json()
 
-    console.log(api_url)
-
-    // Grab videoId from response
-    const videoId = data.items[0].id.videoId
-
-    console.log(videoId)
-
-    const newMedia = {
-        path: '',
-        displayName: data.items[0].snippet.title,
-        cover: '',
-        id: videoId,
-        artist: data.items[0].snippet.channelTitle,
-        type: 'video'
-    }
-
-    media.push(newMedia)
-    console.log(media)
-
-    // Skip to the latest added song
-    changeMedia(media.length-1);
+    // loop through the results and display them on the screen
+    results = data.items;
+    showResults(data.items);
     // Load video into player
     // player.loadVideoById(videoId, 0, "large")
     // playMusic();
@@ -156,13 +153,17 @@ async function search(){
 
 // Update progress bar and display
 function updateProgressBar() {
-    const { duration, currentTime } = music; // Assuming music is your audio player
-    const progressPercent = (currentTime / duration) * 100;
+    const duration = player.playerInfo.progressState.duration;
+    const current = player.playerInfo.progressState.current;
+    const progressPercent = (current / duration) * 100;
     progress.style.width = `${progressPercent}%`;
 
     const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
-    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
-    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+    durationEl.innerText = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.innerText = `${formatTime(current / 60)}:${formatTime(current % 60)}`;
+
+    // durationEl.textContent = formatTime(duration);
+    // currentTimeEl.textContent = formatTime(current);
 }
 
 
@@ -176,6 +177,7 @@ function setProgressBar(e) {
 // Event handlers for YouTube player
 function onPlayerReady(e) {
     // Optional: Logic when the YouTube player is ready
+    player.setVolume(volume.value);
 }
 
 function onPlayerStateChange(e) {
@@ -187,8 +189,9 @@ function startScrolling() {
     const containerWidth = musicTitleContainer.offsetWidth;
     const titleWidth = musicTitle.offsetWidth;
 
-    if (titleWidth > containerWidth) {
-        musicTitle.style.animation = `scroll ${titleWidth / 50}x linear infinite`;
+    if (titleWidth >= containerWidth) {
+        musicTitle.style.animation = `scroll ${titleWidth / 50}x linear infinite;`;
+        console.log(musicTitle.style.animation);
     }
 }   
 
@@ -198,6 +201,7 @@ playBtn.addEventListener('click', () => {
         player.pauseVideo();
     } else {
         player.playVideo();
+        updateProgressBar()
     }
 });
 
@@ -206,8 +210,52 @@ playBtn.addEventListener('click', togglePlay);
 prevBtn.addEventListener('click', () => changeMedia(-1));
 nextBtn.addEventListener('click', () => changeMedia(1));
 music.addEventListener('timeupdate', updateProgressBar);
+volume.addEventListener('change', () => {
+    player.setVolume(volume.value);
+})
 // playerProgress.addEventListener('click', setProgressBar);
 
 loadMedia(media[musicIndex])
+
+function showResults(data) {
+    document.getElementById("results").innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+      const result = data[i];
+  
+      const oldHtml = document.createElement("div");
+      oldHtml.id = `result_${i}`;
+
+      document.getElementById("results").appendChild(oldHtml);
+
+      const html = document.getElementById(`result_${i}`);
+      html.appendChild(document.getElementById("template").cloneNode(true));
+
+      html.querySelector(".title").innerText = result.snippet.title;
+      html.querySelector(".author").innerText = result.snippet.channelTitle;
+  
+      html.querySelector("img").src = result.snippet.thumbnails.default.url;
+  
+      html.onclick = load;
+  
+      html.querySelector('div').id = '';
+    }
+    console.log(results);
+  }
+
+const load = (event) => {
+    event.preventDefault();
+    let video = results[event.currentTarget.id.split('_')[1]];
+    const newMedia = {
+        path: "",
+        displayName: video.snippet.title,
+        cover: "",
+        id: video.id.videoId,
+        artist: video.snippet.channelTitle,
+        type: "video",
+      };
+
+    media.push(newMedia);
+    changeMedia(media.length - 1);
+}
 
 startScrolling();
